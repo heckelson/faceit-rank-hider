4dcdb8721cbc817227d621cd8ea91742f5e3ae4e
heckelson <26191361+heckelson@users.noreply.github.com>
2021-02-02 23:00:16 +0100

    fixed not hiding on first load

--- faceit-rank-hider.js
+++ faceit-rank-hider.js
@@ -1,14 +1,7 @@
+/* Faceit Rank Hider by heckelson */
 
-console.log("Faceit rank hider loaded.");
-
-window.onload = function() {
-	console.log("Window loaded.");
-	
-	removeFaceitRanks();
-}
-
-function removeFaceitRanks() {
-	var banned_ids = [
+/* each of the 10 possible levels has its own id starting with f_icon_. */
+var banned_ids = [
 		"f_icon_ee1c08a8", //  1
 		"f_icon_230d8c21", //  2
 		"f_icon_32f1620f", //  3
@@ -19,16 +12,16 @@ function removeFaceitRanks() {
 		"f_icon_0e5c44d1", //  8
 		"f_icon_e6c73405", //  9
 		"f_icon_aff4d555", // 10
-	];
+];
 
+window.onload = function() {
+	console.log("Using Faceit Rank Hider by heckelson. ");
+	removeFaceitRankIcons(banned_ids);
+}
+
+/* Sets opacity of all ids which are passed as an array to 0.*/
+function removeFaceitRankIcons(banned_ids) {
 	for (var each_id of banned_ids) {
-		var icon = document.getElementById(each_id);
-		if (icon != null && icon != undefined) {
-			icon.remove();
-			console.log("Removing Rank icon: " + each_id + "...");
-		}
-		else {
-			console.log(each_id + " not found.");
-		}
+		document.getElementById(each_id).style.opacity = 0;
 	}
 }
